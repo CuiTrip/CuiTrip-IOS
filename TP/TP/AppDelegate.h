d0d70621ca149730dfbe29f381dcee3c432b85aa
akaDealloc <jayson.xu@foxmail.com>
2015-05-26 11:43:31 +0800

    add files

---
+++ TP/TP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TP
+//
+//  Created by moxin on 15/5/26.
+//  Copyright (c) 2015年 VizLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TP/TP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TP
+//
+//  Created by moxin on 15/5/26.
+//  Copyright (c) 2015年 VizLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
