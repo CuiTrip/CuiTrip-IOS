ad56658747d809c43fd34f2c1f41b79710f1c42e
akaDealloc <jayson.xu@foxmail.com>
2015-05-29 11:52:33 +0800

    add prefix header file

---
+++ TP/TPTheme.h
@@ -0,0 +1,23 @@
+//
+//  TPTheme.h
+//  TP
+//
+//  Created by moxin on 15/5/29.
+//  Copyright (c) 2015年 VizLab. All rights reserved.
+//
+
+#ifndef TP_TPTheme_h
+#define TP_TPTheme_h
+
+namespace TP {
+    
+    
+    
+}
+
+
+#define TPDefaultColor  [UIColor colorWithGray]
+
+
+
+#endif
