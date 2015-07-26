07802f825d11bd7d0ae156deeab185dd35401bde
不铭 <buming.zl@taobao.com>
2015-07-26 17:16:43 +0800

    trip order

--- TP/业务/旅程/详情/view/TPTripDetailSubView.m
+++ TP/业务/旅程/详情/view/TPTripDetailSubView.m
@@ -11,6 +11,7 @@
 
 #import "TPTripDetailSubView.h"
 #import "TPTripDetailItem.h"
+#import "O2OStarView.h"
 
 @interface TPTripDetailSubView()
 
@@ -22,6 +23,8 @@
 @property(nonatomic,strong) UILabel* titleLabel;
 @property(nonatomic,strong) UIImageView* addressIcon;
 @property(nonatomic,strong) UILabel* addressLabel;
+@property (nonatomic,strong) O2OStarView* starView;
+@property (nonatomic,strong) UITextView *textView;
 
 @end
 
@@ -29,9 +32,7 @@
 
 - (id)initWithFrame:(CGRect)frame
 {
-    
     self = [super initWithFrame:frame];
-    
     if (self) {
         
         //todo...
@@ -46,15 +47,6 @@
         self.nameLabel = [TPUIKit label:[TPTheme grayColor] Font:ft(16.0f)];
         self.nameLabel.textAlignment = NSTextAlignmentCenter;
         [self addSubview:self.nameLabel];
-        
-        self.contactBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        self.contactBtn.layer.cornerRadius = 10.0;
-        self.contactBtn.layer.borderWidth = 1.0;
-        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-        [self.contactBtn setTitleColor: [TPTheme themeColor] forState:UIControlStateNormal];
-        CGColorRef borderColorRef = CGColorCreate(colorSpace,(CGFloat[]){36 / 255.0, 194 / 255.0, 213 / 255.0, 1});
-        self.contactBtn.layer.borderColor = borderColorRef;
-        [self addSubview:self.contactBtn];
         
         self.titleLabel = [TPUIKit label:[TPTheme blackColor] Font:ft(16.0f)];
         self.titleLabel.textAlignment = NSTextAlignmentCenter;
@@ -80,8 +72,19 @@
     [self.headIcon sd_setImageWithURL:__url(tripDetailModel.insiderHeadPic) placeholderImage:__image(@"girl.jpg")];
     //服务提供者nick
     self.nameLabel.text = tripDetailModel.insiderNickName;
-    //联系服务者按钮
-    [self.contactBtn setTitle:@"联系他" forState:UIControlStateNormal];
+    if ([self.tripDetailModel.status intValue] != kOrderFinished && [self.tripDetailModel.status intValue] != kOrderInvalid)
+    {
+        //联系服务者按钮
+        self.contactBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        self.contactBtn.layer.cornerRadius = 10.0;
+        self.contactBtn.layer.borderWidth = 1.0;
+        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+        [self.contactBtn setTitleColor: [TPTheme themeColor] forState:UIControlStateNormal];
+        CGColorRef borderColorRef = CGColorCreate(colorSpace,(CGFloat[]){36 / 255.0, 194 / 255.0, 213 / 255.0, 1});
+        self.contactBtn.layer.borderColor = borderColorRef;
+        [self addSubview:self.contactBtn];
+        [self.contactBtn setTitle:@"联系他" forState:UIControlStateNormal];
+    }
     //服务title
     self.titleLabel.text = tripDetailModel.serviceName;
     //服务地址
@@ -94,8 +97,13 @@
     self.poster.frame = CGRectMake(0.0f, 0.0f, self.frame.size.width, 160.0f);
     self.headIcon.frame = CGRectMake((self.frame.size.width - 65.0f) / 2, self.poster.vzBottom - 32.5f, 65.0f, 65.0f);
     self.nameLabel.frame = CGRectMake(0.0f, self.headIcon.vzBottom + 10.0f, self.frame.size.width, 18.0f);
-    self.contactBtn.frame = CGRectMake((self.frame.size.width - 105.0f) / 2, self.nameLabel.vzBottom + 10.0f, 105.0f, 44.0f);
-    self.titleLabel.frame = CGRectMake(0.0f, self.contactBtn.vzBottom + 30.0f, self.frame.size.width, 18.0f);
+    float bottom = self.nameLabel.vzBottom;
+    if (self.contactBtn != nil) {
+        bottom += 10.0f;
+        self.contactBtn.frame = CGRectMake((self.frame.size.width - 105.0f) / 2, bottom, 105.0f, 44.0f);
+        bottom += 44.0f;
+    }
+    self.titleLabel.frame = CGRectMake(0.0f, bottom + 30.0f, self.frame.size.width, 18.0f);
     self.addressIcon.frame = CGRectMake((self.frame.size.width - 44.0f) / 2, self.titleLabel.vzBottom + 13.0f, 14.0f, 14.0f);
     self.addressLabel.frame = CGRectMake(self.addressIcon.vzRight + 5, self.addressIcon.vzTop + 2, 30.0f, 10.0f);
 }
