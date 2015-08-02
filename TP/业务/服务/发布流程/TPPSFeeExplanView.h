a3c75b52d7fd815d3abd2c7cdd20eda51ec78aec
不铭 <buming.zl@taobao.com>
2015-08-02 16:34:53 +0800

    fee explan view

---
+++ TP/业务/服务/发布流程/TPPSFeeExplanView.h
@@ -0,0 +1,12 @@
+//
+//  TPPSFeeExplanView.h
+//  TP
+//
+//  Created by zhou li on 15/8/2.
+//  Copyright (c) 2015年 VizLab. All rights reserved.
+//
+
+
+@interface TPPSFeeExplanView : UIView
+
+@end
---
+++ TP/业务/服务/发布流程/TPPSFeeExplanView.m
@@ -0,0 +1,31 @@
+//
+//  TPPSFeeExplanView.m
+//  TP
+//
+//  Created by zhou li on 15/8/2.
+//  Copyright (c) 2015年 VizLab. All rights reserved.
+//
+
+#import "TPPSFeeExplanView.h"
+
+@interface TPPSFeeExplanView()
+
+
+@end
+
+
+@implementation TPPSFeeExplanView
+
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    
+    return self;
+}
+
+
+@end
