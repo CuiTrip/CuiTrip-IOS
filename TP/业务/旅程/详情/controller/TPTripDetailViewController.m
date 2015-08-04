0ccca0d1fde836d702e72fbad7d0d880531e33eb
贝贝 <jhw1314@gmail.com>
2015-08-04 10:39:23 +0800

    版本1.1.0封包

--- TP/业务/旅程/详情/controller/TPTripDetailViewController.m
+++ TP/业务/旅程/详情/controller/TPTripDetailViewController.m
@@ -397,7 +397,7 @@
         else if (indexPath.row == 2)
         {
             cell.textLabel.text = @"服务费用";
-            cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ %@", self.tripDetailModel.orderPrice, self.tripDetailModel.moneyType];
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ %@", self.tripDetailModel.orderPrice, self.tripDetailModel.payCurrency];
         }
         else if (indexPath.row == 3)
         {
