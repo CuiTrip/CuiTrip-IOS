26658222637e1aa3ebb5f8f0ec1cc94fb60b7aca
贝贝 <jhw1314@gmail.com>
2015-08-04 14:16:11 +0800

    last

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,102 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// INTULocationManager
+#define COCOAPODS_POD_AVAILABLE_INTULocationManager
+#define COCOAPODS_VERSION_MAJOR_INTULocationManager 3
+#define COCOAPODS_VERSION_MINOR_INTULocationManager 0
+#define COCOAPODS_VERSION_PATCH_INTULocationManager 3
+
+// Pingpp/Alipay
+#define COCOAPODS_POD_AVAILABLE_Pingpp_Alipay
+#define COCOAPODS_VERSION_MAJOR_Pingpp_Alipay 2
+#define COCOAPODS_VERSION_MINOR_Pingpp_Alipay 0
+#define COCOAPODS_VERSION_PATCH_Pingpp_Alipay 6
+
+// Pingpp/Core
+#define COCOAPODS_POD_AVAILABLE_Pingpp_Core
+#define COCOAPODS_VERSION_MAJOR_Pingpp_Core 2
+#define COCOAPODS_VERSION_MINOR_Pingpp_Core 0
+#define COCOAPODS_VERSION_PATCH_Pingpp_Core 6
+
+// Pingpp/Wx
+#define COCOAPODS_POD_AVAILABLE_Pingpp_Wx
+#define COCOAPODS_VERSION_MAJOR_Pingpp_Wx 2
+#define COCOAPODS_VERSION_MINOR_Pingpp_Wx 0
+#define COCOAPODS_VERSION_PATCH_Pingpp_Wx 6
+
+// ReactiveCocoa
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa 5
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 0
+
+// ReactiveCocoa/Core
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 5
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 0
+
+// ReactiveCocoa/UI
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_UI
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_UI 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_UI 5
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_UI 0
+
+// ReactiveCocoa/no-arc
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 5
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 0
+
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage 2
+
+// SDWebImage/Core
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 2
+
+// TMCache
+#define COCOAPODS_POD_AVAILABLE_TMCache
+#define COCOAPODS_VERSION_MAJOR_TMCache 2
+#define COCOAPODS_VERSION_MINOR_TMCache 1
+#define COCOAPODS_VERSION_PATCH_TMCache 0
+
+// Toast
+#define COCOAPODS_POD_AVAILABLE_Toast
+#define COCOAPODS_VERSION_MAJOR_Toast 2
+#define COCOAPODS_VERSION_MINOR_Toast 4
+#define COCOAPODS_VERSION_PATCH_Toast 0
+
+// UMengAnalytics-NO-IDFA
+#define COCOAPODS_POD_AVAILABLE_UMengAnalytics_NO_IDFA
+#define COCOAPODS_VERSION_MAJOR_UMengAnalytics_NO_IDFA 3
+#define COCOAPODS_VERSION_MINOR_UMengAnalytics_NO_IDFA 5
+#define COCOAPODS_VERSION_PATCH_UMengAnalytics_NO_IDFA 9
+
+// WCAlertView
+#define COCOAPODS_POD_AVAILABLE_WCAlertView
+#define COCOAPODS_VERSION_MAJOR_WCAlertView 1
+#define COCOAPODS_VERSION_MINOR_WCAlertView 0
+#define COCOAPODS_VERSION_PATCH_WCAlertView 1
+
+// Debug build configuration
+#ifdef DEBUG
+
+  // VZInspector
+  #define COCOAPODS_POD_AVAILABLE_VZInspector
+  #define COCOAPODS_VERSION_MAJOR_VZInspector 0
+  #define COCOAPODS_VERSION_MINOR_VZInspector 0
+  #define COCOAPODS_VERSION_PATCH_VZInspector 6
+
+#endif
